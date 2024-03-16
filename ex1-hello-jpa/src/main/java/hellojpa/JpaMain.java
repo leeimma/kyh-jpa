ad59245129070d92780d2a5b41c854edccc12886
imina <leeimma@naver.com>
2024-03-16 21:16:12 +0900

    [jpa basic] section 3

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,18 +18,38 @@ public class JpaMain {
 
         try {
 
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("hello!!");
+            {
+                Member member = em.find(Member.class, 150L);
+                member.setName("hi");
+            }
 
-            // 영속
-            em.persist(member);
+            {
+                // 영속
+                Member member1 = new Member(150L, "A");
+                Member member2 = new Member(150L, "B");
 
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
+                em.persist(member1);
+                em.persist(member2);
+            }
 
-            System.out.println("findMember2 == findMember1 = " + (findMember2 == findMember1));
+
+            /* 4.
+            {
+                // 비영속
+                Member member = new Member();
+                member.setId(101L);
+                member.setName("hello!!");
+
+                // 영속
+                em.persist(member);
+
+                Member findMember1 = em.find(Member.class, 101L);
+                Member findMember2 = em.find(Member.class, 101L);
+
+                System.out.println("findMember2 == findMember1 = " + (findMember2 == findMember1));
+            }
+            */
+
 
             // 3.
 //            List<Member> result = em.createQuery("select m from Member as m", Member.class)
