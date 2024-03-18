f95efc13b70738ebf5f10c9aea06d85eba0808cf
imina <leeimma@naver.com>
2024-03-18 23:05:19 +0900

    [jpa basic] section 4

--- ex1-hello-jpa/src/main/java/jpabook/jpashop/domain/JpaMain.java
+++ ex1-hello-jpa/src/main/java/jpabook/jpashop/domain/JpaMain.java
@@ -1,4 +1,4 @@
-package hellojpa;
+package jpabook.jpashop.domain;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
---
+++ ex1-hello-jpa/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+//@Table(name = "MBR")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
